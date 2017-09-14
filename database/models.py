908ae7b0ae9a4f995c5b15384694d30285f79625
junaid <junaid1460@gmail.com>
2017-09-14 10:14:23 +0530

    changed somefields and added somemore tables

--- database/models.py
+++ database/models.py
@@ -5,21 +5,49 @@ from django.db import models
 
 #staffs
 class Staff(models.Model):
-    first_name = models.CharField(max_length = 100)
-    last_name = models.CharField(max_length = 100)
-    date_of_birth = models.DateField()
+    _first_name = models.CharField(max_length = 100)
+    _last_name = models.CharField(max_length = 100)
+    _date_of_birth = models.DateField()
+    def __str__(self):
+        return self._first_name + ", "+ self._last_name
+
+
 
 #departments
 class Department(models.Model):
-    name = models.CharField(max_length = 300)
+    _name = models.CharField(max_length = 300, default = None)
 
-#Academic years
+#Academic years <- (department)
 class AcademicYear(models.Model):
-    year = models.IntegerField()
-    tag_name = models.CharField(max_length = 300)
+    _department = models.ForeignKey(Department, default = None )
+    _year = models.IntegerField()
+    _tag_name = models.CharField(max_length = 300)
 
 #student Batches
 class Batch(models.Model):
-    name = models.CharField(max_length = 300)
-    year = models.DateField()
+    _name = models.CharField(max_length = 300)
+    _year = models.DateField()
+
+
     
+#lecturer <- (staff & department)
+class Lecturer(models.Model):
+    _is = models.ForeignKey(Staff)
+    _department =  models.ForeignKey(Department)
+
+#period <- AcademicYear
+class Period(models.Model):
+    _academic_year = models.ForeignKey(AcademicYear)
+
+#subjects <- (Department & Period)
+class Subject(models.Model):
+    _department = models.ForeignKey(Department)
+    _period = models.ForeignKey(Period)
+
+# SubjectAllotted <-(lecturer & subject & ...)
+class SubjectAllotted(models.Model):
+    _lecturer = models.ForeignKey(Lecturer)
+    _subject = models.ForeignKey(Subject)
+
+
+
